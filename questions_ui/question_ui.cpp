c0c9060f6dcbae3365f6c386065773381af0b78f
Zoja Hordyńska <01178224@pw.edu.pl>
2023-05-26 16:12:17 +0200

    Improved question UI.

--- questions_ui/question_ui.cpp
+++ questions_ui/question_ui.cpp
@@ -3,11 +3,19 @@
 #include <iostream>
 #include <cstdint>
 #include <cmath>
+#include <algorithm>
+#include <random>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 extern char _binary_font_arial_ttf_start[];
 extern char _binary_font_arial_ttf_end[];
 
-QuestionUI::QuestionUI(sf::RenderWindow &window, std::string question, std::string answer1, std::string answer2)
+static std::uniform_int_distribution<unsigned short> pick;
+static std::random_device rd;
+static std::default_random_engine re(rd());
+
+QuestionUI::QuestionUI(sf::RenderWindow &window, Enemy& new_enemy)
 {
     if(!background.loadFromFile("scroll_bgrd.png"))
     {
@@ -20,31 +28,53 @@ QuestionUI::QuestionUI(sf::RenderWindow &window, std::string question, std::stri
     {
         throw std::invalid_argument("font load error");
     }
-
     text[0].setFont(font);
     text[0].setCharacterSize(80);
     text[0].setFillColor(sf::Color::Black);
-    text[0].setString(question);
+    text[0].setString(new_enemy.get_name() + " pyta: " + new_enemy.get_content_fromid(0));
     text[0].setPosition(sf::Vector2f(window.getView().getSize().x / 2 - text[0].getGlobalBounds().width / 2 , window.getView().getSize().y / (MAX_NUMBER_OF_ITEMS + 1) * 1));
 
     text[1].setFont(font);
     text[1].setCharacterSize(80);
     text[1].setFillColor(sf::Color::Blue);
-    text[1].setString(answer1);
     text[1].setPosition(sf::Vector2f(window.getView().getSize().x / 2 - text[1].getGlobalBounds().width / 2, window.getView().getSize().y / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+
+    if ((pick(re) & 1) == 0)
+    {
+        text[2].setString(new_enemy.get_wanswer_fromid(0));
+        text[1].setString(new_enemy.get_ganswer_fromid(0));
+    }
+    else
+    {
+        text[1].setString(new_enemy.get_wanswer_fromid(0));
+        text[2].setString(new_enemy.get_ganswer_fromid(0));
+    }
 
     text[2].setFont(font);
     text[2].setCharacterSize(80);
     text[2].setFillColor(sf::Color::Black);
-    text[2].setString(answer2);
+
     text[2].setPosition(sf::Vector2f(window.getView().getSize().x / 2 - text[2].getGlobalBounds().width / 2, window.getView().getSize().y / (MAX_NUMBER_OF_ITEMS + 1) * 3));
 
     selected_index = 1;
+
 }
 
 int QuestionUI::get_pressed_item()
 {
     return selected_index;
+}
+
+void QuestionUI::set_index(int new_index)
+{
+    if (new_index < MAX_NUMBER_OF_ITEMS && new_index >= 0)
+    {
+        selected_index = new_index;
+    }
+    else
+    {
+        throw std::invalid_argument("wrong index number");
+    }
 }
 
 sf::Text& QuestionUI::get_text(int index)
@@ -92,5 +122,104 @@ void QuestionUI::move_down()
         text[selected_index].setFillColor(sf::Color::Black);
         selected_index++;
         text[selected_index].setFillColor(sf::Color::Blue);
+    }
+}
+
+void QuestionUI::exec(Enemy& enemy, Player& player, sf::RenderWindow &window)
+{
+    QuestionUI q_ui(window, enemy);
+    int i = 0;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyReleased)
+            {
+                if (event.key.code == sf::Keyboard::Up)
+                {
+                    q_ui.move_up();
+                }
+                else if (event.key.code == sf::Keyboard::Down)
+                {
+                    q_ui.move_down();
+                }
+                else if ((event.key.code == sf::Keyboard::Return) && (q_ui.get_text(0).getString() != "Press space to continue") && (q_ui.get_text(0).getString() != "I will let you go... for now"))
+                {
+                    if (q_ui.get_pressed_item() == 1)
+                    {
+                        if (q_ui.get_text(1).getString() == enemy.get_ganswer_fromid(i))
+                        {
+                            q_ui.get_text(1).setString("Correct!");
+                            q_ui.get_text(0).setString("Press space to continue");
+                        }
+                        else
+                        {
+                            q_ui.get_text(1).setFillColor(sf::Color::Red);
+                            q_ui.get_text(1).setString("Wrong!");
+                            player.set_hp(player.get_hp() - enemy.get_attack_damage());
+                            q_ui.get_text(0).setString("Press space to continue");
+                        }
+                    }
+                    else if (q_ui.get_pressed_item() == 2)
+                    {
+                        if (q_ui.get_text(2).getString() == enemy.get_ganswer_fromid(i))
+                        {
+                            q_ui.get_text(2).setString("Correct!");
+                            q_ui.get_text(0).setString("Press space to continue");
+                        }
+                        else
+                        {
+                            q_ui.get_text(2).setFillColor(sf::Color::Red);
+                            q_ui.get_text(2).setString("Wrong!");
+                            player.set_hp(player.get_hp() - enemy.get_attack_damage());
+                            q_ui.get_text(0).setString("Press space to continue");
+                        }
+                    }
+                }
+                else if (event.key.code == sf::Keyboard::Space && q_ui.get_text(0).getString() == "Press space to continue")
+                {
+                    if (i == (enemy.number_of_elements() - 1))
+                    {
+                        q_ui.get_text(1).setString("Press  escape to exit");
+                        q_ui.get_text(0).setFillColor(sf::Color::Blue);
+                        q_ui.get_text(1).setFillColor(sf::Color::Black);
+                        q_ui.get_text(0).setString("I will let you go... for now");
+                        q_ui.get_text(2).setString("");
+                        enemy.set_done(true);
+                    }
+                    else
+                    {
+                        q_ui.get_text(0).setString(enemy.get_name() + " pyta: " + enemy.get_content_fromid(i+1));
+                        q_ui.get_text(1).setFillColor(sf::Color::Blue);
+                        if ((pick(re) & 1) == 0)
+                        {
+                            q_ui.get_text(1).setString(enemy.get_ganswer_fromid(i+1));
+                            q_ui.get_text(2).setString(enemy.get_wanswer_fromid(i+1));
+                        }
+                        else
+                        {
+                            q_ui.get_text(2).setString(enemy.get_ganswer_fromid(i+1));
+                            q_ui.get_text(1).setString(enemy.get_wanswer_fromid(i+1));
+                        }
+                        q_ui.get_text(2).setFillColor(sf::Color::Black);
+                        q_ui.set_index(1);
+                        i++;
+                    }
+                }
+                else if (event.key.code == sf::Keyboard::Escape && q_ui.get_text(0).getString() == "I will let you go... for now")
+                {
+                    window.close();
+                }
+            }
+        }
+        window.clear();
+        window.setView(sf::View(sf::FloatRect(0.f, 0.f, window.getSize().x, window.getSize().y)));
+        q_ui.draw(window);
+        window.display();
     }
 }
--- questions_ui/question_window.cpp
+++ questions_ui/question_window.cpp
@@ -1,86 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include <algorithm>
+#include <random>
+#include <cstdint>
 #include "question_ui.h"
-#include "question.h"
-#include "player.h"
-#include "enemy.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-    Question question("Ile to 2+2?", "3", "4", 1);
-    QuestionUI q_ui(window, question.get_content(), question.get_good_answear(), question.get_wrong_answear());
-    Player player("Zoja", 10, 1, 1);
+    sf::RenderWindow window(sf::VideoMode(1400, 1000), "SFML works!");
+    Question question1("Ile to 2+1?", "3", "4", 1);
+    Question question2("Ile to 2+2?", "4", "1", 2);
+    Question question3("Ile to 2+3?", "5", "6", 3);
     Enemy enemy("Pani z dziekanatu", 2, 1, 1);
+    enemy.set_questions(question1);
+    enemy.set_questions(question2);
+    enemy.set_questions(question3);
+    QuestionUI q_ui(window, enemy);
+    Player player("Zoja", 10, 1, 1);
+
     // enemy ma wektor z trzema pytaniami (obiekt Question)
-    // okno powinno przechodzic po trzech obiektach, brac z nich pytanie i odpowiedzi, pokazywac na ekran
-    // dla kazdego ruchu gracza sprawdzac czy odpowiedz jest poprawna - jesli tak to powinno przejsc do nastepnego
-    // jesli nie to zabrac zycie i przejsc do nastepnego
-    // sprawdzic czy skonczyly sie obiekty w wektorze - jesli tak to mark enemy jako done (wtedy powinien zniknac z mapy)
+    // okno przechodzi po trzech obiektach, bierze z nich pytanie i odpowiedzi, pokazuje na ekranie
+    // dla kazdego ruchu gracza sprawdza czy odpowiedz jest poprawna - jesli tak to przechodzi do nastepnego
+    // jesli nie to zabiera zycie i przechodzi do nastepnego
+    // sprawdza czy skonczyly sie obiekty w wektorze - jesli tak to markuje enemy jako done (wtedy powinien zniknac z mapy)
 
-    //for dla elementow wektora enemy, tyle ile ich jest
-    //pokazanie kazdego z pytan, sprawdzenie odpowiedzi - jesli indeks dobry (albo zly) to i++ i zmienia tekst
-    //jak dojdzie do konca to window.close()
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            else if (event.type == sf::Event::KeyReleased)
-            {
-                if (event.key.code == sf::Keyboard::Up)
-                {
-                    q_ui.move_up();
-                }
-                else if (event.key.code == sf::Keyboard::Down)
-                {
-                    q_ui.move_down();
-                }
-                else if (event.key.code == sf::Keyboard::Return)
-                {
-                    if (q_ui.get_pressed_item() == 1)
-                    {
-                        if (q_ui.get_text(1).getString() == question.get_good_answear())
-                        {
-                            q_ui.get_text(1).setString("Correct!");
-                        }
-                        else
-                        {
-                            q_ui.get_text(1).setFillColor(sf::Color::Red);
-                            q_ui.get_text(1).setString("Wrong!");
-                            player.set_hp(player.get_hp() - enemy.get_attack_damage());
-                            std::cout << player.get_hp() << std::endl;
-                        }
-
-                    }
-                    else if (q_ui.get_pressed_item() == 2)
-                    {
-                        if (q_ui.get_text(2).getString() == question.get_good_answear())
-                        {
-                            q_ui.get_text(2).setString("Correct!");
-                        }
-                        else
-                        {
-                            q_ui.get_text(2).setFillColor(sf::Color::Red);
-                            q_ui.get_text(2).setString("Wrong!");
-                            player.set_hp(player.get_hp() - enemy.get_attack_damage());
-                            std::cout << player.get_hp() << std::endl;
-                        }
-                    }
-                }
-            }
-        }
-
-        window.clear();
-        window.setView(sf::View(sf::FloatRect(0.f, 0.f, window.getSize().x, window.getSize().y)));
-        q_ui.draw(window);
-        window.display();
-    }
-
+    q_ui.exec(enemy, player, window);
     return 0;
 }
